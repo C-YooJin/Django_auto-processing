75083cf5e9e0d3c3147dc0e2e4baba1f50d89911
C-YooJin <ioi_3312@naver.com>
2019-02-26 17:29:22 +0900

    v 0.5 / link first page and google crawler button'
    '

--- firstpage/urls.py
+++ firstpage/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.first_page, name='first_page'),
+    path('', views.first_page, name = 'first_page'),
+    # path('')
 ]
--- firstpage/views.py
+++ firstpage/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import firstpage
 
 def first_page(request):
-    firstpages = firstpage.objects.all()
+    # firstpages = firstpage.objects.all()
     return render(request, 'firstpage/index_real.html')
+
 # Create your views here.
