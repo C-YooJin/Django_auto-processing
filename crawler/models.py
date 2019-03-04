ace34fcb607580433ef658e69bea5c20c903ea0c
C-YooJin <ioi_3312@naver.com>
2019-03-04 11:50:55 +0900

    v 0.6 / make google crawler's models.py

--- crawler/models.py
+++ crawler/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class google_crawl(models.Model):
+    name = models.CharField(max_length=20, help_text='Name')
+    employee_number = models.CharField(max_length=12, help_text='Employee Number ex) KR62111', primary_key=True)
+    team_name = models.CharField(max_length=20, help_text='- TEAM NAME -')
+    download_type = models.IntegerField()       # Image only -> 0 / url + meta info -> 1
+    crawl_info = models.TextField()
+    keyword = models.CharField(max_length=20, help_text='keyword to crawl images from google ex) dog')
+    max_num = models.IntegerField()
+
 # Create your models here.
