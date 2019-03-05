4be620f6f1823e7c24d03f93ff06807467116db0
USER <user@macsg.local>
2019-03-05 16:26:10 +0900

    connect with db and minor change with model

---
+++ crawler/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from .models import Google_crawl
+from crispy_forms.helper import FormHelper, Layout
+from crispy_forms.bootstrap import InlineRadios, Div
+from crispy_forms.layout import Submit
+#from django.forms.widgets import RadioSelect
+from django.utils.safestring import mark_safe
+
+
+URL_YN = (
+    ('0', 'URL+Meta'),
+    ('1', 'JPG_ONLY')
+)
+
+
+class GoogleForm(forms.ModelForm):
+    #team_name = forms.ChoiceField(choices=TEAMS)
+    #download_type = forms.TypedChoiceField(widget=forms.RadioSelect(attrs={'style': 'display: inline-block'}), choices=URL_YN)
+
+    class Meta:
+        model = Google_crawl
+        fields = ('name', 'employee_number', 'download_type', 'team_name', 'crawl_info', 'keyword', 'max_num')
+        exclude = ('request_date',)
+        #widgets = {'download_type' : forms.RadioSelect()}
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.helper = FormHelper()
+            #self.helper.layout = Layout(Div(InlineRadios('download_type')))
+            self.helper.form_method = 'post'
+            self.helper.add_input(Submit('submit', 'Let\'s Crawl!'))
--- crawler/models.py
+++ crawler/models.py
@@ -13,12 +13,14 @@ URL_YN = (
 )
 
 class Google_crawl(models.Model):
+    request_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20, help_text='Name')
-    employee_number = models.CharField(max_length=12, help_text='Employee Number ex) KR62111', primary_key=True)
+    employee_number = models.CharField(max_length=12, help_text='Employee Number ex) KR62111')
     team_name = models.CharField(max_length=20, help_text='- TEAM NAME -', choices=TEAMS)
     download_type = models.CharField(max_length=20, choices=URL_YN)       # Image only -> 0 / url + meta info -> 1
     crawl_info = models.TextField(max_length=200, help_text='데이터가 어떤 연구 / 프로젝트에 사용 될지 말씀해주세요')
     keyword = models.CharField(max_length=20, help_text='keyword to crawl images from google ex) dog')
     max_num = models.IntegerField(help_text='Maximum number of images to download ex) 10000')
+    request_date = models.DateTimeField(auto_now_add=True)
 
 # Create your models here.
