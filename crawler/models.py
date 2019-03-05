49c7bb41efdbc3e88c9c4fcc31628a7e2e844f49
USER <user@macsg.local>
2019-03-05 16:05:00 +0900

    connect with db

--- crawler/models.py
+++ crawler/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 
-class google_crawl(models.Model):
+
+TEAMS = (
+    ('', 'Choose your team...'),
+    ('1', 'NSML'),
+    ('2', 'AI Production')
+)
+
+URL_YN = (
+    ('0', 'URL+Meta'),
+    ('1', 'JPG_ONLY')
+)
+
+class Google_crawl(models.Model):
     name = models.CharField(max_length=20, help_text='Name')
     employee_number = models.CharField(max_length=12, help_text='Employee Number ex) KR62111', primary_key=True)
-    team_name = models.CharField(max_length=20, help_text='- TEAM NAME -')
-    download_type = models.IntegerField()       # Image only -> 0 / url + meta info -> 1
-    crawl_info = models.TextField()
+    team_name = models.CharField(max_length=20, help_text='- TEAM NAME -', choices=TEAMS)
+    download_type = models.CharField(max_length=20, choices=URL_YN)       # Image only -> 0 / url + meta info -> 1
+    crawl_info = models.TextField(max_length=200, help_text='데이터가 어떤 연구 / 프로젝트에 사용 될지 말씀해주세요')
     keyword = models.CharField(max_length=20, help_text='keyword to crawl images from google ex) dog')
-    max_num = models.IntegerField()
+    max_num = models.IntegerField(help_text='Maximum number of images to download ex) 10000')
 
 # Create your models here.
