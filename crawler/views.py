027f20c196e6870b37927ecf09c86a20494d6b67
USER <user@macsg.local>
2019-03-07 14:59:59 +0900

    success message

--- crawler/views.py
+++ crawler/views.py
@@ -7,6 +7,8 @@ from django.views.generic.edit import FormView
 from .download_google_images import google_crawler_real
 from django.shortcuts import render, redirect
 from django.contrib.messages.views import SuccessMessageMixin
+from django.urls.base import reverse_lazy
+from django.contrib import messages
 
 # Create your views here.
 
@@ -14,15 +16,15 @@ class RequestCreateView(SuccessMessageMixin, CreateView):
     model = Google_crawl
     form_class = GoogleForm
     template_name = 'crawler/crawler_google.html'
-    success_url = '/success'
-    success_message = "hey, your request was created successfully"
+    success_url = reverse_lazy('crawler_google')
+    success_message = "your request was sent successfully"
 
     def form_valid(self, form):
         keyword = form.cleaned_data['keyword']
         max_num = form.cleaned_data['max_num']
         save = '/Users/user/Downloads/new/'
         google_crawler_real(save, keyword, max_num)
-        return super(RequestCreateView, self).form_valid(form), redirect('crawler/crawler_google.html')
+        return super(RequestCreateView, self).form_valid(form)
 
 
 def crawler_flickr(request):
